6359e9b3c1039d4f3926c81180152cafe44275d6
tsh005-jpg <damoyfriend505@gmail.com>
2026-03-18 19:36:24 +0600

    page_complete

---
+++ script.js
@@ -0,0 +1,7 @@
+function switchTab(index) {
+    document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+    document.querySelectorAll('.tab-image').forEach(i => i.classList.remove('active'));
+
+    document.querySelectorAll('.tab')[index].classList.add('active');
+    document.querySelectorAll('.tab-image')[index].classList.add('active');
+}
