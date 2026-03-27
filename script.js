79a8ca4d9b7acbc84554a9eb774402615fa22662
tsh005-jpg <damoyfriend505@gmail.com>
2026-03-27 19:12:17 +0600

    100%

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function switchTab(index) {
     document.querySelectorAll('.tab-image')[index].classList.add('active');
 }
 
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.mobile-menu-close');
+const overlay = document.querySelector('.mobile-overlay');
+
+burger.addEventListener('click', () => {
+    mobileMenu.classList.add('active');
+    overlay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+    overlay.classList.remove('active');
+});
